d25d10be736e386f0fed4fac864597cb1f68a420
Amal kr <amalkr2001@gmail.com>
2024-03-03 02:03:02 +0530

    fifth commit

--- index.js
+++ index.js
@@ -5,21 +5,13 @@ const cors = require("cors")
 const { connectDb } = require("./config/dbConnection")
 connectDb()
 const app = express();
-const allowedOrigin = process.env.FRONTEND_ORIGIN || 'http://localhost:3000'; 
 
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigin.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
+app.use( cors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
 
 app.use(express.json());
 app.use('/tasks', require('./Router/taskRouter'))
